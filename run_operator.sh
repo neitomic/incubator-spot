11b83469f5420db358718b2bdcd27a51bb95894a
Tien Nguyen <thanhtien522@gmail.com>
2022-09-03 21:28:05 +0700

    Add operator script

---
+++ run_operator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+data_type=$1
+
+# activating environment
+source /opt/impala/bin/impala-config.sh
+source /opt/incubator-spot/spot-ingest/venv/bin/activate
+
+SPOT_HOME=/opt/incubator-spot
+
+data_date=$(date -d "-10 min" "+%Y%m%d")
+echo "Running operator for $data_type on $data_date"
+
+cd $SPOT_HOME/spot-ml
+./ml_ops.sh $data_date $data_type 0.5 200
+
+
+cd $SPOT_HOME/spot-oa/oa
+python start_oa.py -d $data_date -t $data_type -l 200
+
+
